b6d6ace5e4f242f40661f7740fbf2d6d4cceaf1e
zan <clement.mortas@le-campus-numerique.fr>
2019-11-25 15:30:36 +0100

    first commit

---
+++ src/main/java/com/vehiculerental/backwebservice/BackWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vehiculerental.backwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackWebServiceApplication.class, args);
+	}
+
+}
